5f31c5f0a5340ca87b435dde69530bc63b60023b
Алексей <alexey.demidchenko@lll.kpi.ua>
2020-12-09 09:08:27 +0200

    Add project files.

---
+++ Lab7_Var16/Lab7_Var16.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int c[100000];
+    cin >> n;
+    int s = 0, k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> c[i];
+        if (c[i] % 3 == 0 && c[i] != 0)
+        {
+            k++;
+            s += c[i];
+        }
+    }
+    cout << k << " елементів, сума = " << s << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << c[i] + s << " ";
+    }
+    return 0;
+}
